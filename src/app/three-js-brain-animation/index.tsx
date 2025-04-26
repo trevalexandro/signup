32406356a0babc74e859053d2c955e12c017aae0
Trevor Evans <trevalexandro@gmail.com>
2025-04-26 09:02:02 -0500

    add initial files

---
+++ src/app/three-js-brain-animation/index.tsx
@@ -0,0 +1,40 @@
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import * as THREE from 'three';
+import { Tubes } from './brain-tubes.tsx';
+import { BrainParticles } from './brain-particles.tsx';
+import { data } from './data';
+import { JSX } from 'react';
+
+function createBrainCurvesFromPaths(): THREE.CatmullRomCurve3[] {
+  const paths = data.economics[0].paths;
+
+  const brainCurves: THREE.CatmullRomCurve3[] = [];
+  paths.forEach(path => {
+    const points: THREE.Vector3[] = [];
+    for (let i = 0; i < path.length; i += 3) {
+      points.push(new THREE.Vector3(path[i], path[i + 1], path[i + 2]));
+    }
+    const tempCurve = new THREE.CatmullRomCurve3(points);
+    brainCurves.push(tempCurve);
+  });
+
+  return brainCurves;
+}
+
+const curves = createBrainCurvesFromPaths();
+
+const Brain = ():JSX.Element => {
+  return (
+    <Canvas camera={{ position: [0, 0, 0.3], near: 0.001, far: 5 }}>
+      <color attach="background" args={['black']} />
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Tubes curves={curves} />
+      <BrainParticles curves={curves} />
+      <OrbitControls />
+    </Canvas>
+  );
+}
+
+export default Brain;
