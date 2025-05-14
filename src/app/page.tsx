d676641dfedc3340032e615652222d197af1160c
Trevor Evans <trevor.evans@ds2.com>
2025-05-14 07:07:28 -0500

    add collapsible menu for mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,12 @@ import Brain from '@/app/three-js-brain-animation';
 import LearnMore from "./components/learn-more";
 import Signup from './components/signup';
 import SplashButtons from './components/splash-buttons';
-import DrawerMenu from './components/drawer-menu';
+import CollapsibleMenu from './components/collapsible-menu';
+import { useState } from 'react';
 
 export default function Home() {
+  const [collapsibleMenuOpen, setCollapsibleMenuOpen] = useState(false);
+
   const getBrain = (displayClassName: string, screenSize: string = '') => {
     return (
       <div className={`w-full h-full ${displayClassName}`}>
@@ -17,13 +20,13 @@ export default function Home() {
 
   return (
     <div className="h-full w-full">
-      <div className='w-inherit flex flex-row justify-between md:flex-col items-center xl:hidden'>
-        <div className='invisible md:hidden'>
-          <DrawerMenu />
+      <div className={`w-inherit flex ${collapsibleMenuOpen ? 'flex-col flex-start' : 'flex-row'} justify-between md:flex-col items-center xl:hidden`}>
+        <div className={`${collapsibleMenuOpen ? 'hidden' : 'invisible'} md:hidden`}>
+          <CollapsibleMenu openChange={setCollapsibleMenuOpen} />
         </div>
         <p className='text-3xl md:text-5xl lg:text-7xl'>Automind System</p>
         <div className='md:hidden'>
-          <DrawerMenu />
+          <CollapsibleMenu openChange={setCollapsibleMenuOpen} />
         </div>
         <div className='hidden md:block xl:hidden'>
           <SplashButtons />
