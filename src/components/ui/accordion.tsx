2110357eb693b5e55f1066ef0835524085172ba3
Trevor Evans <trevalexandro@gmail.com>
2025-05-14 18:01:42 -0500

    make menu icon white

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -41,7 +41,7 @@ function AccordionTrigger({
         {...props}
       >
         {children}
-        <Menu className="text-muted-foreground pointer-events-none size-4 shrink-0 translate-y-0.5 transition-transform duration-200" />
+        <Menu stroke="white" className="text-muted-foreground pointer-events-none size-4 shrink-0 translate-y-0.5 transition-transform duration-200" />
       </AccordionPrimitive.Trigger>
     </AccordionPrimitive.Header>
   )
